fce29933bf9d9c9734d967920ff110e16e30849d
Elminur Ablimit <elminur226@gmail.com>
2022-03-01 22:17:17 -0800

    US2_AC01_Send Message part is done

--- src/test/java/com/nextbasecrm/tests/UserStory5_Elminur.java
+++ src/test/java/com/nextbasecrm/tests/UserStory5_Elminur.java
@@ -24,7 +24,7 @@ public class UserStory5_Elminur {
     }
 
     @Test
-    public void sendMassageWithoutBody() {
+    public void sendMassageWithoutBody() throws InterruptedException {
         // login
         CRM_Utilities.crm_login(driver, "hr34@cydeo.com", "UserUser");
         //located messageTab then click
@@ -37,11 +37,11 @@ public class UserStory5_Elminur {
         WebElement messageBody = driver.findElement(By.xpath("//body[@contenteditable='true']"));
 
 
-
-//       // click send button without text
+        // click send button without text
         driver.switchTo().defaultContent();
         WebElement sendButton = driver.findElement(By.id("blog-submit-button-save"));
         sendButton.click();
+        Thread.sleep(5000);
 
         // verify get warning message: "The message title is not specified"
         WebElement warningMassage = driver.findElement(By.xpath("//span[@class='feed-add-info-text']"));
@@ -49,8 +49,38 @@ public class UserStory5_Elminur {
         String expectedMessage = "The message title is not specified";
         Assert.assertEquals(actualMessage, expectedMessage);
     }
+
+    @Test
+    public void sendMessageToMessageBody() throws InterruptedException {
+        // login page
+        CRM_Utilities.crm_login(driver, "hr34@cydeo.com", "UserUser");
+        WebElement messageTab = driver.findElement(By.xpath("//span[@id='feed-add-post-form-tab-message']"));
+        messageTab.click();
+
+        // located messageBody, and send message
+        WebElement MessageFrame = driver.findElement(By.xpath("//iframe[@class='bx-editor-iframe']"));
+        driver.switchTo().frame(MessageFrame);
+        WebElement messageBody = driver.findElement(By.xpath("//body[@contenteditable='true']"));
+        messageBody.click();
+        messageBody.sendKeys("hello hello");
+
+        // click send button
+        driver.switchTo().defaultContent();
+        WebElement sendButton = driver.findElement(By.id("blog-submit-button-save"));
+        sendButton.click();
+        Thread.sleep(5000);
+
+        // verify message display in Activity Stream
+        WebElement postedMessage = driver.findElement(By.xpath("(//div[@class='feed-post-text-block-inner-inner'])[1]"));
+        Assert.assertTrue(postedMessage.isDisplayed());
+        //Thread.sleep(5000);
+
+
+    }
+
     @AfterMethod
     public void tearDown() {
         driver.close();
     }
 }
+
